388254da5f61c2e43245f2a9d4e2ce4b9e71e853
Adunolami <abinatitilayo2@gmail.com>
2024-07-12 01:11:11 -0700

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,76 +1,50 @@
 #!/usr/bin/python3
-"""
-N queens problem
-- Approach: Backtracking
-    - Use backtracking to find all possible paths
-    - Check if the path is valid
-        - Check if the column is valid
-        - Check if the positive diagonal is valid
-        - Check if the negative diagonal is valid
-    - Add the queen's location to the path
-    - Continue if the path is valid until we reach the end of the board
-    - Backtrack if the path is not valid and try another path
-    - Add path to the result if we reach the end of the board
-- Analysis:
-    - Time: O(n!) - n is the number of queens
-        - We have n choices for the first queen, n - 1 choices for the
-          second queen,n - 2 choices for the third queen, etc.
-    - Space: O(n^2) - n is the number of queens
-"""
+""" N queens puzzle, challenge of placing N non attacking queens
+on a NxN chessboard
+This program solves the N queens problem """
 
-import sys
+from sys import argv
 
 
-def n_queens(n):
-    """ N queens solution """
-    queens, res = [], []
-    cols, positive_diag, negative_diag = set(), set(), set()
-
-    def backtrack(row, n, queens):
-        """ Backtracking function """
-        if row == n:
-            res.append(queens[:])
-            return
-        for col in range(n):
-            if (col in cols or row + col in positive_diag or
-                    row - col in negative_diag):
-                continue
-            cols.add(col)
-            positive_diag.add(row + col)
-            negative_diag.add(row - col)
-            queens.append([row, col])
-            backtrack(row + 1, n, queens)
-
-            cols.remove(col)
-            positive_diag.remove(row + col)
-            negative_diag.remove(row - col)
-            queens.pop()
-    backtrack(0, n, queens)
-    return res
+def is_NQueen(cell: list) -> bool:
+    """ False if not N Queen, True if N Queen """
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
+            return False
+    return True
 
 
-def check_args(n):
-    """ Check if n is a valid argument """
-    if not n.isdigit():
-        print("N must be a number")
-        exit(1)
-    if int(n) < 4:
-        print("N must be at least 4")
-        exit(1)
+def solve_NQueens(dimension: int, row: int, cell: list, output: list):
+    """ Return result of N Queens recursively """
+    if row == dimension:
+        print(output)
+    else:
+        for column in range(0, dimension):
+            cell.append(column)
+            output.append([row, column])
+            if (is_NQueen(cell)):
+                solve_NQueens(dimension, row + 1, cell, output)
+            cell.pop()
+            output.pop()
 
 
-def main():
-    """ Main function """
-    args = sys.argv
-    if len(args) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    n = args[1]
-    check_args(n)
-    solutions = n_queens(int(n))
-    for solution in solutions:
-        print(solution)
-
-
-if __name__ == "__main__":
-    main()
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+try:
+    N = int(argv[1])
+except BaseException:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+else:
+    output = []
+    cell = 0
+    solve_NQueens(int(N), cell, [], output)
